ea4011f0ca03c8f482225c94a2a6bc74c926f53f
Kanstantsin Kulak <kkulak24@gmail.com>
2021-04-28 22:43:33 +0200

    feat: complete DateFormatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,27 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.ResolverStyle
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        return try {
+            LocalDate.parse(
+                "$year-$month-$day",
+                DateTimeFormatter.ofPattern("uuuu-M-d")
+                    .withResolverStyle(ResolverStyle.STRICT)
+            ).format(
+                DateTimeFormatter.ofPattern(
+                    "dd MMMM, EEEE",
+                    Locale("ru")
+                )
+            )
+        } catch (e: Exception) {
+            println(e)
+            "Такого дня не существует"
+        }
     }
 }
